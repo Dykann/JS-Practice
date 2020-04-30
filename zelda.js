beb53e2758e892579cd7493ae2430c6e5f95c394
Dykann <titi@air-de-dykann.home>
2020-04-30 16:24:06 +0200

    :rocket: launch app

---
+++ zelda.js
@@ -0,0 +1,17 @@
+let pdv = 100;
+
+const vieHTML = document.querySelector(".vie");
+
+vieHTML.textContent = pdv;
+
+const link = document.querySelector(".link-img");
+
+// quand on clique sur Link
+link.addEventListener("click", function () {
+  if (pdv === 0) {
+    vieHTML.textContent = "DEAD";
+    return null;
+  }
+  pdv = pdv - 10;
+  vieHTML.textContent = pdv;
+});
