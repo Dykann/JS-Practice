76fa648c6f7b3f5e43edb5ef74a856b6d352d2f2
Dykann <dykann3@hotmail.fr>
2020-05-12 17:59:09 +0200

    :fire: Array Destructuring + .map()

---
+++ Jump/jump.js
@@ -0,0 +1,17 @@
+const title = document.querySelector("h1");
+
+const letterArray = [...title.textContent];
+
+console.log(letterArray);
+
+const spans = letterArray
+  .map(function (letter) {
+    return `<span class="jump">${letter}</span>`;
+  })
+  .join("");
+
+title.innerHTML = spans;
+
+Array.prototype.salut = function () {
+  console.log("salut");
+};
