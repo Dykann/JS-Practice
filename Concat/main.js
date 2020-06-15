04006420606289ff5b9f3c63e657f7d789ba81e2
Dykann <dykann3@hotmail.fr>
2020-06-15 23:51:05 +0200

    :heavy_plus_sign: Try concat method

---
+++ Concat/main.js
@@ -0,0 +1,9 @@
+const array1 = ["Julien", "Emmy", "Eden", "Lara"];
+const array2 = ["Ambre", "Ayden", "Mystic"];
+const array3 = array1.concat(array2);
+const array4 = array2.concat(array1);
+
+console.log(array3);
+console.log(array4);
+
+// The concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
