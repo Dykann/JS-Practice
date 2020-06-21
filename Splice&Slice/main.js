b8caaea4ae0f73b94465efda67cc6e3760085255
Dykann <dykann3@hotmail.fr>
2020-06-22 01:57:21 +0200

    :art: Add toggle class (enabled/disabled) for color change

--- Splice&Slice/main.js
+++ Splice&Slice/main.js
@@ -15,3 +15,13 @@ function nonMutatingSlice(cars) {
 let inputCars = ["Bmw", "Mercedes", "Toyota", "Audi", "Tesla"];
 
 console.log(nonMutatingSlice(inputCars));
+
+// Action on button click
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function (e) {
+  console.log(e);
+  document.body.classList.toggle("red");
+  btn.classList.toggle("yellow");
+});
