2247e2b328d0acfcf761cea30cc296873030c873
Dykann <dykann3@hotmail.fr>
2020-09-03 19:57:45 +0200

    :fire: Add a fun transition

--- Random colors generator/main.js
+++ Random colors generator/main.js
@@ -5,6 +5,9 @@ const handleClick = function () {
     "#" + Math.floor(Math.random() * parseInt("FFFFFF", 16)).toString(16);
   btn.style.backgroundColor =
     "#" + Math.floor(Math.random() * parseInt("FFFFFF", 16)).toString(16);
+
+  btn.classList.toggle("btn-style");
+  btn.classList.toggle("transition");
 };
 
 btn.addEventListener("click", handleClick);
--- Toggle class 2/main.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector("button");
-
-btn.addEventListener("click", function () {
-  document.body.classList.toggle("backgroundColor");
-  btn.classList.toggle("btn-style");
-  btn.classList.toggle("transition");
-});
