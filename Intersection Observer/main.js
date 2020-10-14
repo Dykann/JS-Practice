a74679e7f7754f86b44c2bb4ba31e2143ee9f964
Dykann <dykann3@hotmail.fr>
2020-10-14 18:02:31 +0200

    :chart_with_upwards_trend: Build a fun transition on cars with intersection observer

---
+++ Intersection Observer/main.js
@@ -0,0 +1,29 @@
+const firstImg = document.querySelector("img");
+
+function PageTransition() {
+  firstImg.classList.add("active");
+}
+
+firstImg.addEventListener("load", PageTransition);
+
+const images = document.querySelectorAll(".img");
+
+const options = {
+  root: null,
+  threshold: 0,
+  rootMargin: "20px",
+};
+
+const observer = new IntersectionObserver(function (entries) {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.classList.add("active");
+    } else {
+      entry.target.classList.remove("active");
+    }
+  });
+}, options);
+
+images.forEach((image) => {
+  observer.observe(image);
+});
