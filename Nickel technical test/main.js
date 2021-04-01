2d4971f7322481461f9bb04ea734085741511c7d
Dykann <dykann3@hotmail.fr>
2021-04-02 01:36:15 +0200

    Reorganize folders

--- Nickel technical test/main.js
+++
@@ -1,9 +0,0 @@
-const pad = document.querySelectorAll(".cn-button");
-
-function test(arr) {
-  arr.forEach((number) =>
-    document.querySelector(`button[data-cy=codeKeyboard${number}]`).click()
-  );
-}
-
-test([4, 7, 9, 2, 1, 3, 4, 5, 8]);
